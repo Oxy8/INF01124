e6cae3d80272e4770466184d4a4569d72b1acdc0
Oxy8 <34687508+Oxy8@users.noreply.github.com>
2024-07-23 00:24:44 -0300

    Finaliza beecrowd

--- LABS/LAB04/bee1256/src/main.rs
+++ LABS/LAB04/bee1256/src/main.rs
@@ -16,15 +16,10 @@ fn main() {
     for _caso in 0..num_casos {
         
         handle.read_line(&mut buffer).unwrap();
-        let par: Vec<u16> = buffer.trim()
-            .split_whitespace()
-            .map(|v| v.parse::<u16>().unwrap())
-            .collect::<Vec<u16>>();
+        let mut par = buffer.trim().split_whitespace();
+        let divisor: u16 = par.next().unwrap().parse().unwrap();
+        let lista_entrada_size: u16 = par.next().unwrap().parse().unwrap();
         buffer.clear();
-
-        let divisor = par[0];
-        let lista_entrada_size = par[1];
-
 
         handle.read_line(&mut buffer).unwrap();
         let lista_entrada: Vec<u16> = buffer.trim()
@@ -36,29 +31,21 @@ fn main() {
         let mut acc_vec: Vec<Vec<u16>> =  vec![Vec::new(); divisor as usize];
 
         
-        for val_index in 0..lista_entrada_size {
-            
-            let val = lista_entrada[val_index as usize];
-            let resto = (val % divisor);
-
-            acc_vec[resto as usize].push(val);
+        for val in lista_entrada.iter().take(lista_entrada_size as usize) {
+            let resto = val % divisor;
+            acc_vec[resto as usize].push(*val);
         }
 
-        for indice in 0..divisor {
-            print_indice_vec(indice as usize, &acc_vec, &mut buf);
+        for (indice, sublist) in acc_vec.iter().enumerate() {
+            write!(buf, "{} -> ", indice).unwrap();
+            for num in sublist {
+                write!(buf, "{} -> ", num).unwrap();
+            }
+            writeln!(buf, "\\").unwrap();
         }
         writeln!(buf, "").unwrap();
     }
 
     buf.pop();
     print!("{}",buf);
-}
-
-
-fn print_indice_vec(indice: usize, vec: &Vec<Vec<u16>>, buf: &mut String) {
-    write!(buf, "{} -> ", indice).unwrap();
-    for num in &vec[indice] {
-        write!(buf, "{} -> ", num).unwrap();
-    }
-    writeln!(buf, "\\").unwrap();
 }
